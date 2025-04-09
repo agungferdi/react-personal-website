68f1e5816e7597673a008a476627a4e431849a24
Ferdiansyah Muhammad Agung <ferdiansyahmuhammadagung@Ferdiansyahs-MacBook-Pro.local>
2025-04-09 13:13:18 +0700

    update scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AppRoutes from './routes'
+import ScrollToTop from './components/ScrollToTop'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <ScrollToTop />
         <Header />
         <main className="main-content">
           <AppRoutes />
